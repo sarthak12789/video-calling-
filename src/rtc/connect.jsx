1f0a2f84db07ea4f202a7946285ac0826c85834a
sarthak12789 <mrtechy404@gmail.com>
2026-03-19 00:27:52 +0530

    trying other ice servers

--- src/rtc/connect.jsx
+++ src/rtc/connect.jsx
@@ -1,16 +1,28 @@
 class peer {
   constructor() {
     this.peer = new RTCPeerConnection({
-      iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-        {
-          urls: "turn:free.expressturn.com:3478",
-          username: "000000002089219264",
-          credential: "T0FVyW9Zzu7BvJN6lt3MuHevUeA=",
-        },
-      ],
+       iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+
+    {
+      urls: "turn:free.expressturn.com:3478?transport=udp",
+      username: "000000002089219264",
+      credential: "T0FVyW9Zzu7BvJN6lt3MuHevUeA="
+    },
+    {
+      urls: "turn:free.expressturn.com:3478?transport=tcp",
+      username: "000000002089219264",
+      credential: "T0FVyW9Zzu7BvJN6lt3MuHevUeA="
+    },
+    {
+      urls: "turns:free.expressturn.com:5349",
+      username: "000000002089219264",
+      credential: "T0FVyW9Zzu7BvJN6lt3MuHevUeA="
+    }
+  ],
     });
   }
+  
   async getOffer() {
     if (this.peer) {
       const offer = await this.peer.createOffer();
