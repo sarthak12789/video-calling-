bb5a8da7c896b586736ba9aac5ea28e9b00b6690
sarthak12789 <mrtechy404@gmail.com>
2026-03-19 00:09:49 +0530

    added turn server

--- src/rtc/connect.jsx
+++ src/rtc/connect.jsx
@@ -1,38 +1,30 @@
-
-
-class peer{
-    constructor()
-    {
-       
-         this.peer =new RTCPeerConnection(
-            {
-                iceServers:[
-                    {
-                        urls:"stun:stun.l.google.com:19302"
-                    }
-                ]   
-            }
-        )
-        
-
+class peer {
+  constructor() {
+    this.peer = new RTCPeerConnection({
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        {
+          urls: "free.expressturn.com:3478",
+          username: "000000002089218760",
+          credential: "YpzPwoatLFBaMrcg03+XA00gjXY=",
+        },
+      ],
+    });
+  }
+  async getOffer() {
+    if (this.peer) {
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(offer);
+      return offer;
     }
-    async getOffer() {
-        if(this.peer)
-        {
-  const offer = await this.peer.createOffer();
-  await this.peer.setLocalDescription(offer);
-  return offer;
-}}
-        
-    async getanswer()
-    {
-        if(this.peer)
-        {
-            const answer = await this.peer.createAnswer();
-            await this.peer.setLocalDescription(answer);
-            return answer;
-        }
-    }    
-    
+  }
+
+  async getanswer() {
+    if (this.peer) {
+      const answer = await this.peer.createAnswer();
+      await this.peer.setLocalDescription(answer);
+      return answer;
+    }
+  }
 }
 export default new peer();
